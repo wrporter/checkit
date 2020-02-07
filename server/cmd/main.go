053e223fe1441e62fad24bcda08742a32995ea64
Wesley Porter <wesp@qualtrics.com>
2020-02-06 21:29:19 -0700

    Update log time format

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"github.com/wrporter/games-app/server/internal/env"
 	"github.com/wrporter/games-app/server/internal/server"
 	"github.com/wrporter/games-app/server/internal/server/auth"
 	"github.com/wrporter/games-app/server/internal/server/items"
 	"log"
 	"net/http"
+	"time"
 )
 
+type logWriter struct{}
+
+func (writer logWriter) Write(bytes []byte) (int, error) {
+	return fmt.Print(time.Now().UTC().Format("2006-01-02T15:04:05.999Z") + " [DEBUG] " + string(bytes))
+}
+
 func main() {
+	log.SetFlags(0)
+	log.SetOutput(new(logWriter))
+
 	s := server.New()
 	auth.RegisterRoutes(s)
 	items.RegisterRoutes(s)
