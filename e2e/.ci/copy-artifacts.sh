93f8d59fe2eee91fd50213c95a3428c3f27fd5b5
Wesley Porter <wesp@qualtrics.com>
2022-01-11 16:25:23 -0700

    #31: Attempt SSH to remote server

--- e2e/.ci/copy-artifacts.sh
+++ e2e/.ci/copy-artifacts.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
 #set -e
+
 source .ci/config.sh
 
+mkdir -p $(pwd)/results
+
+# Use if docker build produces the results
 #container_id=$(docker create ${TARGET_IMAGE}:${VERSION})
-docker cp ${APP_NAME}:/src/cypress/results/. $(pwd)/results/report
-docker cp ${APP_NAME}:/src/cypress/screenshots/. $(pwd)/results/screenshots
-docker cp ${APP_NAME}:/src/cypress/videos/. $(pwd)/results/videos
-#docker rm -v ${APP_NAME}
+
+docker cp ${APP_NAME}:/src/cypress/results/. $(pwd)/results/report || true
+docker cp ${APP_NAME}:/src/cypress/screenshots/. $(pwd)/results/screenshots || true
+docker cp ${APP_NAME}:/src/cypress/videos/. $(pwd)/results/videos || true
+
+docker rm -v ${APP_NAME}
